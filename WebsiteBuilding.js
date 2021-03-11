0279a0b7f4bccd966616c238748a073bde259f05
Benaya Trabelsi <Benaya7@gmail.com>
2021-03-11 05:48:09 +0200

    3rd commit - almost done

--- WebsiteBuilding.js
+++ WebsiteBuilding.js
@@ -10,6 +10,7 @@ class dataManager {
     await this.getAllWeather();
     await this.getUserData();
   }
+
   async initWeather() {
     await this.getAllWeather();
   }
@@ -87,101 +88,120 @@ data reminder: data=>allUsersData=>
           gender: userData.gender,
           hobby: userData.hobby,
 
-
 */
 class makeTable {
   constructor(data) {
     this.data = data;
     this.table = document.querySelector('.users-table');
   }
+  init() {
+    this.setRowsOnDOM();
+    this.setEventListeners();
+  }
   removeAllData() {
     this.table.removeChild(this.table.getElementsByTagName('tbody')[0]);
   }
   // *check sorting behaviour - does it recognize the attribute parameter? did the binding worked?
-
-  setRowsOnDOM(attribute = id) {
+  //change sorting function for alphabetical comparison vs numberical.
+  setRowsOnDOM(attribute = 'id') {
+    if (this.table.querySelector('tbody')) {
+      this.removeAllData();
+    }
     this.table.insertAdjacentHTML('beforeend', `<tbody></tbody>`);
-    const sorter = (user1, user2) => user1[attribute] - user2[attribute];
-    for (user of this.data.allUsersData.sort(sorter)) {
+
+    this.data.allUsersData.sort((user1, user2) => {
+      if (user1[attribute] > user2[attribute]) {
+        return 1;
+      } else if (user1[attribute] < user2[attribute]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    for (const user of this.data.allUsersData) {
       this.addTableRow(user);
     }
   }
   setFilteredRowsOnDOM(attribute, filterString) {
-    removeAllData();
+    this.removeAllData();
+    this.table.insertAdjacentHTML('beforeend', `<tbody></tbody>`);
+
     const filter = (user) =>
-      user[attribute].match(new RegExp(`^${filterString}`));
-    for (user of this.data.allUsersData.filter(filter)) {
+      new RegExp(`^${filterString}`, 'i').test(user[attribute]);
+    for (const user of this.data.allUsersData.filter(filter)) {
       this.addTableRow(user);
     }
   }
   addTableRow(user) {
-    const innerHTML = `<tr><td>${user.id}</td><td>${user.firstName}</td><td>${user.lastName}</td><td>${user.capsule}</td><td>${user.age}</td><td>${user.city}</td><td>${user.gender}</td><td>${user.hobby}</td></tr> <button class = "first edit">edit</button><button class = "second delete">delete</button>`;
-    this.table.insertAdjacentHTML('beforeend', innerHTML);
+    const innerHTML = `<tr><td>${user.id}</td><td>${user.firstName}</td><td>${user.lastName}</td><td>${user.capsule}</td><td>${user.age}</td><td>${user.city}</td><td>${user.gender}</td><td>${user.hobby}</td><td><button class = "first edit">edit</button></td><td><button class = "second delete">delete</button></td></tr> `;
+    this.table
+      .querySelector('tbody')
+      .insertAdjacentHTML('beforeend', innerHTML);
   }
   setEventListeners() {
     const searchBox = document.querySelector('#searchBox');
 
-    searchBox.addEventListener('keydown', (event) => {
-      const attribute = document.querySelector('#selectSearch');
-      const filterString = event.target.innerText;
+    searchBox.addEventListener('keyup', (event) => {
+      const attribute = document.querySelector('#selectSearch').value;
+      const filterString = event.target.value;
       this.setFilteredRowsOnDOM(attribute, filterString);
     });
-    const editHandlerBind = editHandler.bind(this);
-    const sortHandlerBind = sortHandler.bind(this);
-    const deleteRowBind = deleteRow.bind(this);
+    const editHandlerBind = makeTable.editHandler.bind(this);
+    const sortHandlerBind = makeTable.sortHandler.bind(this);
+    const deleteRowBind = makeTable.deleteRow.bind(this);
     this.table.addEventListener('click', sortHandlerBind);
     this.table.addEventListener('click', editHandlerBind);
     this.table.addEventListener('click', deleteRowBind);
   }
   static sortHandler(event) {
     if (event.target.tagName != 'TH') return;
-    const attribute = event.target.innerText;
-    this.setRowsOnDom(attribute);
+    const attribute = toWeirdCase(event.target.innerText);
+    this.setRowsOnDOM(attribute);
   }
   static editHandler(event) {
     if (event.target.innerText != 'edit') return;
-    const currentRow = event.target.parentElement;
+    const currentRow = event.target.parentElement.parentElement;
     const currentUser = this.data.allUsersData.find(
-      (x) => x.id === parseInt(currentRow.firstChildElement.innerText)
+      (x) => x.id === parseInt(currentRow.firstElementChild.innerText)
     );
-    savedInerHTML = currentRow.innerHTML;
-    currentRow.innerHTML = `<tr><td>${currentUser.id}</td><td><input type="text" id="fname" value="${currentUser.firstName}"></td><td><input type="text" id="lname" value="${currentUser.lastName}"></td><td>$<input type="text" id="capsule" value="${currentUser.capsule}"></td><td><input type="text" id="age" value="${currentUser.age}"></td><td><input type="text" id="city" value="${currentUser.city}"></td><td><input type="text" id="gender" value="${currentUser.gender}"></td><td><input type="text" id="hobby" value="${currentUser.hobby}"></td></tr><button class = "first cancel">cancel</button><button class = "second confirm">confirm</button>`;
+    const savedInerHTML = currentRow.innerHTML;
+    currentRow.innerHTML = `<tr><td>${currentUser.id}</td><td><input type="text" id="fname" value="${currentUser.firstName}"></td><td><input type="text" id="lname" value="${currentUser.lastName}"></td><td>$<input type="text" id="capsule" value="${currentUser.capsule}"></td><td><input type="text" id="age" value="${currentUser.age}"></td><td><input type="text" id="city" value="${currentUser.city}"></td><td><input type="text" id="gender" value="${currentUser.gender}"></td><td><input type="text" id="hobby" value="${currentUser.hobby}"></td><td><button class = "first cancel">cancel</button></td><td><button class = "second confirm">confirm</button></td></tr>`;
     const cancelButton = currentRow.querySelector('.cancel');
     const confirmButton = currentRow.querySelector('.confirm');
     //binding the previous innerHtml to revert to.
-    const revertBind = revert.bind(savedInerHTML);
+    const revertBind = makeTable.revert.bind(savedInerHTML);
     //binding this to make and update chages on.
-    const updateValuesBind = updateValues.bind(this);
-    cancelButton.addEventListener('click', revertBind, (once = true));
-    confirmButton.addEventListener('click', updateValuesBind, (once = true));
+    const updateValuesBind = makeTable.updateValues.bind(this);
+    cancelButton.addEventListener('click', revertBind, { once: true });
+    confirmButton.addEventListener('click', updateValuesBind, { once: true });
   }
   static revert(event) {
     const row = event.target.parentElement;
     row.innerHTML = this;
   }
   static updateValues(event) {
+    const currentRow = event.target.parentElement.parentElement;
     const currentUser = this.data.allUsersData.find(
-      (x) => x.id === parseInt(currentRow.firstChildElement.innerText)
+      (x) => x.id === parseInt(currentRow.firstElementChild.innerText)
     );
-    const rowChildren = event.target.parentElement.children;
-    currentUser.firstName = rowChildren.item(1).firstChildElement.value;
-    currentUser.lastName = rowChildren.item(2).firstChildElement.value;
-    currentUser.capsule = rowChildren.item(3).firstChildElement.value;
-    currentUser.age = rowChildren.item(4).firstChildElement.value;
-    currentUser.city = rowChildren.item(5).firstChildElement.value;
-    currentUser.gender = rowChildren.item(6).firstChildElement.value;
-    currentUser.hobby = rowChildren.item(7).firstChildElement.value;
+    const rowChildren = event.target.parentElement.parentElement.children;
+    currentUser.firstName = rowChildren.item(1).firstElementChild.value;
+    currentUser.lastName = rowChildren.item(2).firstElementChild.value;
+    currentUser.capsule = rowChildren.item(3).firstElementChild.value;
+    currentUser.age = rowChildren.item(4).firstElementChild.value;
+    currentUser.city = rowChildren.item(5).firstElementChild.value;
+    currentUser.gender = rowChildren.item(6).firstElementChild.value;
+    currentUser.hobby = rowChildren.item(7).firstElementChild.value;
     //need to make it happen!
     // this.data.updateDatabaseFromApi();
     this.removeAllData();
     this.setRowsOnDOM();
-    this.setEventListeners();
   }
   static deleteRow(event) {
     if (event.target.innerText != 'delete') return;
-    const currentRow = event.target.parentElement;
+    const currentRow = event.target.parentElement.parentElement;
     //get the id:
-    this.data.removeUser(currentRow.firstChildElement.innerText);
+    this.data.removeUser(currentRow.firstElementChild.innerText);
     this.data.updateDatabaseFromApi();
     this.removeAllData();
     this.setRowsOnDOM();
@@ -194,10 +214,13 @@ async function go() {
     if (!localData) {
       localData = new dataManager();
       await localData.init();
-      localStorage.setItem('all data', localData);
+      localStorage.setItem('all data', JSON.stringify(localData));
     } else {
       localData = JSON.parse(localData);
+      Object.setPrototypeOf(localData, dataManager);
     }
+    const usersTable = new makeTable(localData);
+    usersTable.init();
   } catch (error) {
     console.error(error);
   }
@@ -206,10 +229,19 @@ go();
 
 /* 
 left to do: 
-- activate search and near select.
 - set event listeners: on the table head for sorting.
-- more event listeners: on the table rows for editing.
-- disabling all event listeners from id tds because it can't change.
-- implement a removeUser to the database manager!
 * I assume that there is no need to redo event listeners for the dynamically created elements. I'll check it tommorow.
 */
+
+/* 
+at 4am wednesday: left to do : prevent unwanted behaviour from the table click events.
+at 5:44am: remaining - delete function - when parsing json object back from local storage, it doesn't retrieve back the class functions too. I need to write a function to convert it back. maybe using lodash.
+- also - make weather div show on hover.
+- also - change wrong event listeners behaviour.
+*/
+function toWeirdCase(str) {
+  return str
+    .split(/-|_| /)
+    .map((word, index, array) => (array[0] == word ? word.toLowerCase() : word))
+    .join('');
+}
